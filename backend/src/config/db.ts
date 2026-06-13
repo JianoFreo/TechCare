2e45cdf1da88ed2499f41ed1f00e4552ceceb8f9
Jiano Freo <jianofreo.magtangob@tup.edu.ph>
2026-06-13 20:12:10 +0800

    login function

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -5,7 +5,7 @@ import "dotenv/config";
 const DATABASE_URL = process.env.DATABASE_URL;
 
 if (!DATABASE_URL) {
-    throw new Error("DATABASE_URL is not defined");
+  throw new Error("DATABASE_URL is not defined");
 }
 
 ///////////////==========================for neon db==============================
@@ -13,21 +13,35 @@ if (!DATABASE_URL) {
 export const sql = neon(DATABASE_URL);
 
 export async function connectNeon(): Promise<void> {
-    try {
-        await sql`
+  try {
+    await sql`
             CREATE TABLE IF NOT EXISTS users (
-                id SERIAL PRIMARY KEY,
-                email VARCHAR(255) NOT NULL UNIQUE,
+                user_id SERIAL PRIMARY KEY,
+                username VARCHAR(255) NOT NULL,
                 password VARCHAR(255) NOT NULL,
+                role VARCHAR(50) NOT NULL,
                 created_at DATE NOT NULL DEFAULT CURRENT_DATE
-            )
+            );
+            CREATE TABLE IF NOT EXISTS laboratory_request (
+                patient_id SERIAL PRIMARY KEY,
+                request_id VARCHAR(255) NOT NULL UNIQUE,
+                laboratory_service VARCHAR(255) NOT NULL,
+                status VARCHAR(50) NOT NULL,
+                request_at DATE NOT NULL DEFAULT CURRENT_DATE,
+                request_time TIME NOT NULL DEFAULT CURRENT_TIME,
+                cancelled BOOLEAN NOT NULL DEFAULT FALSE,
+                cancellation_reason TEXT,
+                call_queue_number VARCHAR(255) NOT NULL UNIQUE
+        );
+
+
         `;
 
-        console.log("Database initialized successfully on Neon DB");
-    } catch (error) {
-        console.error("Error initializing DB", error);
-        process.exit(1);
-    }
+    console.log("Database initialized successfully on Neon DB");
+  } catch (error) {
+    console.error("Error initializing DB", error);
+    process.exit(1);
+  }
 }
 
 ///////////////==========================for local db==============================
