e6fb964cb4212f81026a9c6149ca020e49231c1a
Jiano Freo <jianofreo.magtangob@tup.edu.ph>
2026-07-03 03:58:40 +0800

    fixed types

--- frontend/src/users/frontdesk_staff/pages/PatientRegistration.tsx
+++ frontend/src/users/frontdesk_staff/pages/PatientRegistration.tsx
@@ -1,8 +1,28 @@
-function PatientRegistration( ){
-    return(
+
+import Header from "../components/Header"
+type Patient = {
+    patient_id: number;
+    first_name: string;
+    last_name: string;
+    date_of_birth: string;
+    contact_number: string;
+    email: string;
+    address: string;
+    emergency_contact: string;
+    created_at: string;
+    updated_at: string;
+}[];
+type PatientRegistrationProps = {
+    patients: Patient[];
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    loadData: () => Promise<void>;
+};
+function PatientRegistration({ patients, open, setOpen, loadData }: PatientRegistrationProps) {
+    return (
         <div>
             patient registry
-            </div>
+        </div>
     )
 }
 export default PatientRegistration
--- frontend/src/users/frontdesk_staff/pages/QueueManagement.tsx
+++ frontend/src/users/frontdesk_staff/pages/QueueManagement.tsx
@@ -1,5 +1,21 @@
-function QueueManagement( ){
-    return(
+type queue = {
+    queue_id: number;
+    patient_id: number;
+    queue_number: number;
+    doctor_id: number;
+    service_type: string;
+    status: string;
+    created_at: string;
+    updated_at: string;
+}[];
+type QueueManagementProps = {
+    queue: queue;
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    loadData: () => Promise<void>;
+};
+function QueueManagement({ queue, open, setOpen, loadData }: QueueManagementProps) {
+    return (
         <div>
             queue management    
             </div>
