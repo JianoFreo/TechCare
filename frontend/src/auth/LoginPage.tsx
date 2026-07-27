435c41d417e6ad6c4240b3316c0db841e3946f5a
Jiano Freo <jianofreo.magtangob@tup.edu.ph>
2026-07-27 11:58:36 +0800

    kulang ng }

--- frontend/src/auth/LoginPage.tsx
+++ frontend/src/auth/LoginPage.tsx
@@ -12,105 +12,106 @@ function LoginPage() {
 
 
   const login = async () => {
+    try{
+    if (!username || !password) {
+      alert("Please fill in all fields.");
+      return;
+    }
+    setLoading(true);
+    const response = await api.post("/api/auth/login", {
+      username,
+      password,
+    });
+    console.log("Login response:", response.data);
     try {
-      if (!username || !password) {
-        alert("Please fill in all fields.");
-        return;
-      }
-      setLoading(true);
-      const response = await api.post("/api/auth/login", {
-        username,
-        password,
-      });
-      console.log("Login response:", response.data);
-      try {
-        if (!response.data) {
-          alert(response.data.message);
-          setUsername("");
-          setPassword("");
-        }
-        else {
-          const token = response.data.token;
-          localStorage.setItem("token", token);
-          navigate(`/${response.data.user.role}`);
-          console.log(localStorage.getItem("token"));
-        }
-      } catch {
+      if (!response.data) {
         alert(response.data.message);
         setUsername("");
         setPassword("");
-      } finally {
-        setLoading(false);
       }
-    } catch (error) {
-      console.error("Login error:", error);
-      alert("An error occurred during login. Please try again.");
+      else {
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+        navigate(`/${response.data.user.role}`);
+        console.log(localStorage.getItem("token"));
+      }
+    } catch {
+      alert(response.data.message);
+      setUsername("");
+      setPassword("");
+    } finally {
       setLoading(false);
-    };
+    }
+  }catch (error) {
+    console.error("Login error:", error);
+    alert("An error occurred during login. Please try again.");
+    setLoading(false);
+  }
+};
 
-    return (
-      <div className="min-h-screen flex">
-        <LeftSideBackground />
-        <div className="w-full md:w-1/2 flex items-center justify-center ">
-          <div className="w-105 border-2 p-10">
-            <div className="text-center mb-6">
-              <h2 className="text-3xl font-bold text-gray-800">
-                Welcome Back
-              </h2>
+  return (
+    <div className="min-h-screen flex">
+      <LeftSideBackground />
+      <div className="w-full md:w-1/2 flex items-center justify-center ">
+        <div className="w-105 border-2 p-10">
+          <div className="text-center mb-6">
+            <h2 className="text-3xl font-bold text-gray-800">
+              Welcome Back
+            </h2>
 
-              <p className="text-sm text-gray-500 mt-1">
-                Login to TechCare System
-              </p>
-            </div>
-
-            <div className="space-y-4">
-              <input
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Username"
-                className="w-full  p-3 border-2 rounded"
-              />
-
-              <input
-                type={showPassword ? "text" : "password"}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-                className="w-full  p-3 border-2 rounded"
-              />
-
-              <button
-                type="button"
-                onClick={() => setShowPassword(!showPassword)}
-                className="text-sm hover:underline"
-              >
-                {showPassword ? "Hide Password" : "Show Password"}
-              </button>
-              <button
-                onClick={login}
-                className="w-full bg-gray-200 p-3 rounded hover:bg-gray-300 cursor-pointer"
-                disabled={loading}
-              >
-                {loading ? "Logging in..." : "Login"}
-              </button>
-            </div>
-
-            <p className="text-xs text-center text-gray-400 mt-5">
-              forgot password?{" "}
-              <span className="text-indigo-500 cursor-pointer">
-                Send a request
-              </span>
+            <p className="text-sm text-gray-500 mt-1">
+              Login to TechCare System
             </p>
-            {/* <p className="text-xs text-center text-gray-400 mt-5">
+          </div>
+
+          <div className="space-y-4">
+            <input
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+              className="w-full  p-3 border-2 rounded"
+            />
+
+            <input
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              className="w-full  p-3 border-2 rounded"
+            />
+
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="text-sm hover:underline"
+            >
+              {showPassword ? "Hide Password" : "Show Password"}
+            </button>
+            <button
+              onClick={login}
+              className="w-full bg-gray-200 p-3 rounded hover:bg-gray-300 cursor-pointer"
+              disabled={loading}
+            >
+              {loading ? "Logging in..." : "Login"}
+            </button>
+          </div>
+
+          <p className="text-xs text-center text-gray-400 mt-5">
+            forgot password?{" "}
+            <span className="text-indigo-500 cursor-pointer">
+              Send a request
+            </span>
+          </p>
+          {/* <p className="text-xs text-center text-gray-400 mt-5">
             No account yet? {" "}
             <span className="text-indigo-500 cursor-pointer" onClick={() =>navigate("/sign-up")}>
               Sign Up
             </span>
           </p> */}
-          </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  export default LoginPage;
+export default LoginPage;
