5498b9e1954e6ea358c999ca6aa974c9d60cbdd6
Marc Jodel L. Dela Cruz <marcjodeldelacruz@gmail.com>
2026-07-30 16:55:29 +0800

    fix: change return status to prevent bad request for empty arrays

--- backend/src/controllers/labstaff/getRequests.controller.ts
+++ backend/src/controllers/labstaff/getRequests.controller.ts
@@ -3,20 +3,17 @@ import { json, Request, Response } from "express";
 
 export async function getAllLaboratoryRequests(req: Request, res: Response) {
   try {
-    const { status } = req.query;
     const labRequests = await sql`
-        SELECT request_id, patient_id, doctor_id, test_type, status
+        SELECT request_id, patient_id, doctor_id, test_type, status, requested_at, updated_at
         FROM lab_requests
         WHERE is_paid = TRUE AND
-              LOWER(status) = LOWER(${status}) and
+              status != 'Requested' and
               requested_at >= CURRENT_DATE
               AND requested_at < CURRENT_DATE + INTERVAL '1 day'
         ORDER BY updated_at ASC;
     `;
     if (labRequests.length === 0) {
-      return res.status(400).json({
-        message: `There are no laboratory requests found for status: ${status}`,
-      });
+      return res.status(200).json([]);
     }
 
     res.status(200).json(labRequests);
