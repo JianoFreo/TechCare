097cb7ef7b360deab9545dbf5570e539512544bc
Jiano Freo <jianofreo.magtangob@tup.edu.ph>
2026-08-06 15:57:29 +0800

    Refactor login logic to handle errors and reset fields accordingly

--- frontend/src/auth/LoginPage.tsx
+++ frontend/src/auth/LoginPage.tsx
@@ -29,33 +29,30 @@ function LoginPage() {
       alert("Please fill in all fields.");
       return;
     }
-
     setLoading(true);
+    let response;
 
     try {
-      const response = await api.post("/api/auth/login", {
+      response = await api.post("/api/auth/login", {
         username,
         password,
       });
-
-      console.log("RESPONSE:", response);
-      console.log("DATA:", response.data);
-
-      if (response.data.message !== "Login successful") {
-        alert(response.data.message);
-        return;
+      console.log("Login response:", response.data);
+      if (!response.data) {
+        alert(response?.data.message);
+        setUsername("");
+        setPassword("");
       }
-
-      const token = response.data.token;
-      const role = response.data.user.role;
-
-      localStorage.setItem("token", token);
-      localStorage.setItem("role", role);
-
-      navigate(`/${role}`);
-    } catch (error) {
-      console.error("ACTUAL ERROR:", error);
-      alert("Login failed.");
+      else {
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+        navigate(`/${response.data.user.role}`);
+        console.log(localStorage.getItem("token"));
+      }
+    } catch {
+      alert(response?.data.message);
+      setUsername("");
+      setPassword("");
     } finally {
       setLoading(false);
     }
