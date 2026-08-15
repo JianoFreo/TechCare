cb34e030ed564a1ba976c632746f1371623da816
Marc Jodel L. Dela Cruz <marcjodeldelacruz@gmail.com>
2026-08-15 13:50:36 +0800

    fix: update password comparison to use correct field and improve formatting in login function

--- backend/src/controllers/auth/postRequests.controller.ts
+++ backend/src/controllers/auth/postRequests.controller.ts
@@ -21,17 +21,23 @@ export async function login(req: Request, res: Response) {
     if (!user) {
       return res.status(200).json({ message: "Invalid username" });
     }
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password_hash);
     if (!isMatch) {
-      return res.status(200).json({ message: "Invalid password" }); 
+      return res.status(200).json({ message: "Invalid password" });
     }
-    const token = jwt.sign({user_id: user.user_id}, ENV.JWT_SECRET, {expiresIn : "5min"}); // it goingg to make a token out of user id
+    const token = jwt.sign({ user_id: user.user_id }, ENV.JWT_SECRET, {
+      expiresIn: "5min",
+    }); // it goingg to make a token out of user id
     // this has to be an object because jwt.sign expects an object as the first argument, not a string. So we wrap user.user_id in an object with a key of user_id.
-    const refreshToken = jwt.sign({user_id: user.user_id}, ENV.JWT_REFRESH_TOKEN, {expiresIn : "1d"}); // it goingg to make a token out of user id
+    const refreshToken = jwt.sign(
+      { user_id: user.user_id },
+      ENV.JWT_REFRESH_TOKEN,
+      { expiresIn: "1d" },
+    ); // it goingg to make a token out of user id
 
-
-    
-    res.status(200).json({ message: "Login successful", user, token, refreshToken });
+    res
+      .status(200)
+      .json({ message: "Login successful", user, token, refreshToken });
     //   {
     //   "message": "Login successful",
     //   "user": {
@@ -45,6 +51,7 @@ export async function login(req: Request, res: Response) {
     //   "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
     // }
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 }
