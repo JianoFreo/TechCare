fcc6dab26bde5472aaba8973015da724eea64a28
Jiano Freo <jianofreo.magtangob@tup.edu.ph>
2026-08-22 04:58:50 +0800

    Add logout endpoint to auth controller and route

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import { login } from '../controllers/auth/postRequests.controller.js';
+import { logout } from '../controllers/auth/updateRequests.controller.js';
+import authMiddleware from '../middlewares/auth.middleware.js';
 
 const router = Router();
+router.use()
 router.post('/login', login);
+router.post('/logout',authMiddleware, logout);
 
 export default router;
