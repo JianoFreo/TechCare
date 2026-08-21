fbfb41cf78429b47726b5b76f3c62751f61d9450
Jiano Freo <jianofreo.magtangob@tup.edu.ph>
2026-08-21 21:49:41 +0800

    Refactor Header component to use UserHeader for user details
    - Moved user details into a separate `UserHeader` component
    - Used `localStorage` to fetch user data
    - Updated button onClick to call `loadData` function

--- frontend/src/components/Header/UserHeader.tsx
+++ frontend/src/components/Header/UserHeader.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { ChevronDown } from "lucide-react";
 
 function UserHeader() {
+  const storedUser = localStorage.getItem("user");
+  const user = storedUser ? JSON.parse(storedUser) : null;
+
   return (
-    <div>UserHeader</div>
-  )
+    <div className="flex items-center gap-3 border-l border-gray-300 px-2">
+      <img
+        src={user?.profile_photo || "/assets/images.jpg"}
+        alt="Profile"
+        className="w-12 h-12 rounded-full border"
+      />
+
+      <div className="flex flex-col">
+        <h1 className="text-lg font-semibold">
+          {user?.first_name}{" "}
+          {user?.middle_name ? `${user.middle_name} ` : ""}
+          {user?.last_name}
+        </h1>
+
+        <h2 className="text-gray-500">
+          {user?.email}
+        </h2>
+      </div>
+
+      <ChevronDown
+        size={20}
+        strokeWidth={1}
+        className="cursor-pointer"
+      />
+    </div>
+  );
 }
 
-export default UserHeader
+export default UserHeader;
