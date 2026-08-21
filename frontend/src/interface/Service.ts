7cd4f5a91f1e51a433dc27a0b901c507a3edab94
Jiano Freo <jianofreo.magtangob@tup.edu.ph>
2026-08-22 05:32:58 +0800

    feat(frontend): add Service interface and update Admin component

---
+++ frontend/src/interface/Service.ts
@@ -0,0 +1,11 @@
+export type Service = {
+    id: number;
+    service_id: string;
+    service_name: string;
+    price: number;
+    active: boolean,
+    service_type: string,
+    room: string,
+    created_at: string;
+    updated_at: string;
+};
